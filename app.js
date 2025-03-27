6c4318bab53c4904e017d51dfe27afd8c9e2ef38
360-ahemenjeho <159141014+360-ahemenjeho@users.noreply.github.com>
2025-03-27 21:08:57 +0100

    perf(done)

--- app.js
+++ app.js
@@ -1,75 +1,114 @@
-const gameBoard = Array(9).fill('')
+function GameController() {
+  let gameBoard = Array(9).fill('')
 
-const winningCombination = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+  const boardEl = document.querySelector('#board')
+  const statusEl = document.querySelector('#status')
+  const resetEl = document.querySelector('#reset')
 
-let currentPlayer = 'X'
+  const winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
-function playGame(index) {
-  if (gameBoard[index]) {
-    console.error('Already filled!')
-    return
+  let currentPlayer = 'X'
+  let isPlaying = true
+
+  function switchPlayer() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
   }
 
-  const checkRes = checkWinner(currentPlayer, gameBoard)
-  console.log('CHECK RESPONSE: ', checkRes)
-  if (checkRes.end) {
-    console.log(checkRes.msg)
-    return
-  }
+  function checkWinner() {
+    // Check if current user has filled any of these combinations
+    const winner = winningCombination.some((combination) =>
+      combination.every((i) => gameBoard[i] === currentPlayer)
+    )
 
-  gameBoard[index] = currentPlayer
-  currentPlayer = switchPlayer(currentPlayer)
-}
+    // Check if game is a tie (all cells filled)
+    const isTie = gameBoard.every((cell) => cell !== '')
 
-function switchPlayer(current) {
-  return current === 'X' ? 'O' : 'X'
-}
+    // Determine return object
+    if (winner) {
+      return {
+        end: true,
+        msg: `Player ${currentPlayer} wins!`
+      }
+    }
 
-function checkWinner(player, gameBoard = []) {
-  // Check if current user has filled any of these combinations
-  const winner = winningCombination.some((combination) =>
-    combination.every((i) => gameBoard[i] === player)
-  )
+    if (isTie) {
+      return {
+        end: true,
+        msg: 'Tie. Both players win!'
+      }
+    }
 
-  // Check if game is a tie (all cells filled)
-  const isTie = gameBoard.every((cell) => cell !== '')
-
-  // Determine return object
-  if (winner) {
     return {
-      end: true,
-      msg: `Player ${player} wins!`
+      end: false,
+      msg: ''
     }
   }
 
-  if (isTie) {
-    return {
-      end: true,
-      msg: 'Tie. Both players win!'
+  function updateStatus(msg = '') {
+    if (msg) statusEl.textContent = msg
+    else statusEl.textContent = `Current player: ${currentPlayer}`
+  }
+
+  function resetGame() {
+    gameBoard = Array(9).fill(null)
+
+    const cells = boardEl.children
+    for (let cell of cells) {
+      cell.textContent = ''
     }
+
+    currentPlayer = 'X'
+    updateStatus()
   }
 
-  return {
-    end: false,
-    msg: ''
+  function playGame(e) {
+    const currEl = e.target
+    const cellIndex = parseInt(currEl.dataset.index)
+
+    if (gameBoard[cellIndex] || !isPlaying) return
+
+    gameBoard[cellIndex] = currentPlayer
+    currEl.textContent = currentPlayer
+
+    const checkRes = checkWinner()
+
+    if (checkRes.end) {
+      updateStatus(checkRes.msg)
+      isPlaying = false
+      return
+    }
+
+    switchPlayer()
+    updateStatus()
   }
+
+  function initGame() {
+    boardEl.innerHTML = ''
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement('div')
+      cell.dataset.index = i
+      cell.addEventListener('click', playGame)
+      boardEl.appendChild(cell)
+    }
+
+    resetEl.addEventListener('click', resetGame)
+    updateStatus()
+  }
+
+  // PUBLIC API
+  return { init: initGame }
 }
 
-function __init__() {
-  for (let i = 0; i < 9; i++) {
-    playGame(i)
-  }
-}
-
-__init__()
-
-console.log('GAME BOARD: ', gameBoard)
+document.addEventListener('DOMContentLoaded', () => {
+  const game = GameController()
+  game.init()
+})
